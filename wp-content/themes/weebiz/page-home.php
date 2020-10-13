cdeb39497bbe439b68eabbec5bde0737b0504910
Mike Jackson-Potter <nzmikejp@gmail.com>
2020-10-14 09:07:13 +1300

    Profile tweeks

--- wp-content/themes/weebiz/page-home.php
+++ wp-content/themes/weebiz/page-home.php
@@ -7,31 +7,7 @@
     <?php get_template_part('features')?>
 
     <!-- Team Members -->
-    <div class="container team">
-      <div class="page-header-title">
-        <h2 class="heading-title text-center">Team Members</h2>
-      </div>
-      <div class="team-members mtb-50">
-        <div class="row">
-            <?php
-
-                $args = array('post_type' => 'profile');
-                // The Query
-                $the_query = new WP_Query( $args );
-
-                // The Loop
-                while ( $the_query->have_posts() ) {
-                    $the_query->the_post();
-                    get_template_part('partials/content', 'profile');
-                }
-
-                /* Restore original Post Data */
-                wp_reset_postdata();
-
-            ?>
-            </div>
-        </div>
-    </div>
+    <?php get_template_part('profile')?>
 
 
 <?php get_footer()?>
